8e5bd577ae407a6ba063003a81b1502f156a3700
Lieu <lieu8336@saskpolytech.ca>
2023-04-11 15:50:35 -0600

    authentic commit

---
+++ db/models/User.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: false}
+})
+
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
--- routes/routes.js
+++ routes/routes.js
@@ -1,34 +1,43 @@
 const express = require('express');
+const passport = require('passport');
 
 const musicController = require('../controllers/controller');
+const authController = require('../controllers/authController');
+
+const auth = require('../auth');
 
 const router = express();
 
 
-
-
 // routes
-    //GET ALL
-    router.get('/music', musicController.getAllMusic )
-    
-    
-    
-    //Get by id
-    
-    router.get('/music/:id', musicController.getMusiceById)
-    
-    //create album
-    
-    router.post('/music', musicController.createAlbum);
-    
-    //update album
-    //make put request
-    //we need an id from client
-    router.put('/music/:id', musicController.updateAlbum );
-        //use mongoose to validate the data and save it to the database
-    
-    //delete route
-    
-    router.delete('/music/:id', musicController.deleteAlbum );
+
+
+//auth
+router.post('/register', authController.register);
+router.post('/login', passport.authenticate('local',{session:false}) ,authController.login);
+
+
+//GET ALL
+router.get('/music', auth.verifyUser ,musicController.getAllMusic );
+
+
+
+//Get by id
+
+router.get('/music/:id',auth.verifyUser , musicController.getMusiceById);
+
+//create album
+
+router.post('/music',auth.verifyUser , musicController.createAlbum);
+
+//update album
+//make put request
+//we need an id from client
+router.put('/music/:id',auth.verifyUser , musicController.updateAlbum );
+    //use mongoose to validate the data and save it to the database
+
+//delete route
+
+router.delete('/music/:id',auth.verifyUser , musicController.deleteAlbum );
 
 module.exports = router;
